91a5cf7f82d2b72d9ffc839f759aff7fa142a893
Pratham <agrawalpratham2002@gmail.com>
2024-08-08 23:28:28 +0530

    added titles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,14 @@
+import Head from "next/head";
+
 export default function about() {
-  return <div className="h-screen pt-16">About</div>;
+  return (
+    <>
+      <Head>
+        <title>About Me</title>
+      </Head>
+      <section id="main">
+        <div className="h-screen pt-16">About</div>
+      </section>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,14 @@ import CaseStudy from "@/components/Home/CaseStudy/CaseStudy";
 import Hero from "@/components/Home/Hero/Hero";
 import IndustryWork from "@/components/Home/IndustryWork/IndustryWork";
 import Testimonials from "@/components/Home/Testimonials/Testimonials";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Balaji UXUI</title>
+      </Head>
       <section id="content" className="">
         <Hero />
         <IndustryWork />
